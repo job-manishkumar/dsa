216b84fac843e59b24067f3002adcc64882d0f80
manish <job.manishkumar@gmail.com>
2024-06-07 09:29:35 +0530

    find out all sub array sum2

---
+++ src/main/java/scaler/day32/FindAllSubArraySum2.java
@@ -0,0 +1,28 @@
+package scaler.day32;
+
+public class FindAllSubArraySum2 {
+
+	/*You are given an integer array A of length N.
+	You have to find the sum of all subarray sums of A.
+	More formally, a subarray is defined as a contiguous part of an array which we can obtain by deleting zero or more elements from either end of the array.
+	A subarray sum denotes the sum of all the elements of that subarray.
+			Note : Be careful of integer overflow issues while calculations. Use appropriate datatypes.*/
+
+	public static int getSum(int[] a){
+		int totalSum = 0;
+		for(int i=0;i<a.length;i++){
+			int currSum = 0;
+			for(int j=i;j<a.length;j++){
+				currSum+=a[j];
+				totalSum+=currSum;
+			}
+		}
+		return totalSum;
+	}
+
+	public static void main(String[] args) {
+		int[] a ={1,2,3,4,5};
+		int totalSum = getSum(a);
+		System.out.println(totalSum);
+	}
+}
