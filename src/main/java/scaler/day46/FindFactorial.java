267b5876cd6d464c121b3b4d387ec004fb3e4fe1
manish <job.manishkumar@gmail.com>
2024-06-07 10:23:34 +0530

    find K number factorial

---
+++ src/main/java/scaler/day46/FindFactorial.java
@@ -0,0 +1,38 @@
+package scaler.day46;
+
+public class FindFactorial {
+
+	/*Write a program to find the factorial of the given number A using recursion.
+	Note: The factorial of a number N is defined as the product of the numbers from 1 to N.*/
+
+	//using loop
+	public static int getFactorial(int num) {
+		int fact = 1;
+		for (int i = 1; i <= num; i++) {
+			fact = fact * i;
+		}
+		return fact;
+	}
+
+	//using recursion
+	public static int getFactorial1(int num) {
+
+		//base case
+		if (num == 1) {
+			return 1;
+		}
+		//main logic
+		return getFactorial1(num - 1) * num;
+	}
+
+	public static void main(String[] args) {
+
+		int a = 4;
+		int fact = getFactorial(a);
+		System.out.println(fact);
+
+		System.out.println("using recursion");
+		int fact1 = getFactorial1(a);
+		System.out.println(fact1);
+	}
+}
