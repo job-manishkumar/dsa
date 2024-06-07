b2b4c5182e3423c3469aa9fa0265e7ca15c72a53
manish <job.manishkumar@gmail.com>
2024-06-07 09:27:41 +0530

    Time to Equality

---
+++ src/main/java/scaler/day29/additionalproblems/TimeToEquality.java
@@ -0,0 +1,28 @@
+package scaler.day29.additionalproblems;
+
+public class TimeToEquality {
+
+	/*Given an integer array A of size N. In one second, you can increase the value of one element by 1.
+	Find the minimum time in seconds to make all elements of the array equal.*/
+
+	public static int getEquality(int[] a) {
+		int maxElement = Integer.MIN_VALUE;
+		for (int i : a) {
+			if (i > maxElement) {
+				maxElement = i;
+			}
+		}
+		int count = 0;
+		for (int i : a) {
+			count += (maxElement - i);
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+
+		int[] a = {2, 4, 1, 3, 2};
+		int count = getEquality(a);
+		System.out.println(count);
+	}
+}
