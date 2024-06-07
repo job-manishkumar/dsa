fa85a828b18cc0e29eaa2675a9ed527076683e64
manish <job.manishkumar@gmail.com>
2024-06-07 09:28:17 +0530

    Count Sub Array Sum

---
+++ src/main/java/scaler/day31/additionalproblems/CountingSubArraysSum.java
@@ -0,0 +1,33 @@
+package scaler.day31.additionalproblems;
+
+public class CountingSubArraysSum {
+
+	/*Given an array A of N non-negative numbers and a non-negative number B,
+	you need to find the number of sub-arrays in A with a sum less than B.
+	We may assume that there is no overflow.*/
+
+	//brute force approach
+	public static int getSum(int[] a, int b) {
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			int sum = 0;
+			for (int j = i; j < a.length; j++) {
+				sum += a[j];
+				if (sum < b) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+
+	public static void main(String[] args) {
+		int[] a = {1, 11, 2, 3, 15};
+		int b = 10;
+		int count = getSum(a, b);
+		System.out.println(count);
+	}
+
+
+}
